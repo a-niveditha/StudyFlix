c1f798258d9d58a71906d1a4eea9d360d803569b
nivi <niveditha6174@gmail.com>
2026-02-22 23:49:56 +0530

    added notes page

---
+++ notes.js
@@ -0,0 +1,10 @@
+function toggleDone(item) {
+    item.classList.toggle('done');
+  }
+
+  document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', () => {
+      document.querySelectorAll('.nav-link').forEach(l => l.classList.remove('active'));
+      link.classList.add('active');
+    });
+  });
